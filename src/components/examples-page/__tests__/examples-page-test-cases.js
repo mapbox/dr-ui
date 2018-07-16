31eadf238df0dedd13482e8fe51b0ca345a39c8c
Colleen <colleen.j.mcginnis@gmail.com>
2018-07-15 21:10:36 -0700

    add example page layout

---
+++ src/components/examples-page/__tests__/examples-page-test-cases.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import { ExamplesPage } from '../examples-page';
+import { CardContainer } from '../../card-container/card-container';
+import { Card } from '../../card/card';
+
+const testCases = {};
+
+testCases.basic = {
+  component: ExamplesPage,
+  description: 'Basic',
+  props: {
+    frontMatter: {
+      title: 'Title',
+      description: 'Description.'
+    },
+    cardContainers: [
+      <CardContainer
+        title="Container title one"
+        path="#container-title-one"
+        fullWidthCards={false}
+        cards={[
+          <Card
+            key="0"
+            title="Example one"
+            path="path"
+            description="description"
+            thumbnail={
+              <div
+                className="relative h120 mb12"
+                style={{
+                  backgroundImage:
+                    "url('https://i.giphy.com/media/A9lgUYVqLeRb2/giphy.webp')",
+                  backgroundSize: '100% auto',
+                  backgroundPosition: 'center'
+                }}
+              />
+            }
+          />,
+          <Card
+            key="1"
+            title="Example two"
+            path="path"
+            description="description"
+            thumbnail={
+              <div
+                className="relative h120 mb12"
+                style={{
+                  backgroundImage:
+                    "url('https://i.giphy.com/media/A9lgUYVqLeRb2/giphy.webp')",
+                  backgroundSize: '100% auto',
+                  backgroundPosition: 'center'
+                }}
+              />
+            }
+          />
+        ]}
+      />,
+      <CardContainer
+        title="Container title two"
+        path="#container-title-two"
+        fullWidthCards={false}
+        cards={[
+          <Card
+            key="0"
+            title="Example one"
+            path="path"
+            description="description"
+            thumbnail={
+              <div
+                className="relative h120 mb12"
+                style={{
+                  backgroundImage:
+                    "url('https://i.giphy.com/media/A9lgUYVqLeRb2/giphy.webp')",
+                  backgroundSize: '100% auto',
+                  backgroundPosition: 'center'
+                }}
+              />
+            }
+          />,
+          <Card
+            key="1"
+            title="Example two"
+            path="path"
+            description="description"
+            thumbnail={
+              <div
+                className="relative h120 mb12"
+                style={{
+                  backgroundImage:
+                    "url('https://i.giphy.com/media/A9lgUYVqLeRb2/giphy.webp')",
+                  backgroundSize: '100% auto',
+                  backgroundPosition: 'center'
+                }}
+              />
+            }
+          />,
+          <Card
+            key="2"
+            title="Example two"
+            path="path"
+            description="description"
+            thumbnail={
+              <div
+                className="relative h120 mb12"
+                style={{
+                  backgroundImage:
+                    "url('https://i.giphy.com/media/A9lgUYVqLeRb2/giphy.webp')",
+                  backgroundSize: '100% auto',
+                  backgroundPosition: 'center'
+                }}
+              />
+            }
+          />
+        ]}
+      />
+    ]
+  }
+};
+
+export default { testCases };
