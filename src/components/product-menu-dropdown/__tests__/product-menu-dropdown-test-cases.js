88e3e7e76a647f8074233dc49dd76bcb60b1f533
Dan Swick <dan.swick@gmail.com>
2018-07-17 10:50:49 -0700

    Add ProductMenu, ProductMenuDropdown components. Add ProductMenuItems data. (#14)

---
+++ src/components/product-menu-dropdown/__tests__/product-menu-dropdown-test-cases.js
@@ -0,0 +1,14 @@
+import { ProductMenuDropdown } from '../product-menu-dropdown';
+import { ProductMenuItems } from '../../../data/product-menu-items';
+
+const testCases = {};
+
+testCases.basic = {
+  component: ProductMenuDropdown,
+  description: 'Product menu dropdown',
+  props: {
+    categories: ProductMenuItems
+  }
+};
+
+export default { testCases };
---
+++ src/components/product-menu/__tests__/product-menu-test-cases.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { ProductMenu } from '../product-menu';
+import { ProductMenuItems } from '../../../data/product-menu-items';
+import { ProductMenuDropdown } from '../../product-menu-dropdown/product-menu-dropdown';
+
+const testCases = {};
+const locationTestItems = [
+  {
+    productCategory: 'Foo',
+    icon: 'map',
+    products: [
+      {
+        url: '/mapbox-gl-js/',
+        name: 'Foo1'
+      },
+      {
+        url: '/ios-sdk/maps/',
+        name: 'Foo2'
+      }
+    ]
+  },
+  {
+    productCategory: 'Bar',
+    icon: 'compass',
+    products: [
+      {
+        url: '/ProductMenu',
+        name: 'ProductMenu'
+      }
+    ]
+  }
+];
+
+function renderMenu(items) {
+  return <ProductMenuDropdown categories={items} />;
+}
+
+function renderBoring() {
+  return <img src={'https://i.giphy.com/media/A9lgUYVqLeRb2/giphy.webp'} />;
+}
+
+testCases.mapboxMenu = {
+  component: ProductMenu,
+  description: 'Mapbox product menu',
+  props: {
+    productName: 'Mapbox product names',
+    children: renderMenu(ProductMenuItems)
+  }
+};
+
+testCases.locationMenu = {
+  component: ProductMenu,
+  description: "Fake items, but one matches this test case's location",
+  props: {
+    productName: 'Items in here',
+    children: renderMenu(locationTestItems)
+  }
+};
+
+testCases.arbitraryElement = {
+  component: ProductMenu,
+  description: 'Product menu with something arbitrary in it',
+  props: {
+    productName: 'Pizza pie',
+    children: renderBoring()
+  }
+};
+
+export default { testCases };
