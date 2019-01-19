663614cb154c6b1d7be8823917718cb92bf919f8
Katy DeCorah <decorah@mapbox.com>
2019-01-18 21:49:14 -0500

    replace IconButton with Icon and Button and PopoverTrigger

--- src/components/back-to-top-button/back-to-top-button.js
+++ src/components/back-to-top-button/back-to-top-button.js
@@ -1,27 +1,41 @@
 import React from 'react';
-import IconButton from '@mapbox/mr-ui/icon-button';
+import Button from '@mapbox/mr-ui/button';
+import Icon from '@mapbox/mr-ui/icon';
+import PopoverTrigger from '@mapbox/mr-ui/popover-trigger';
 
 export default class BackToTopButton extends React.Component {
+  getPopoverContent = () => {
+    return <div className="txt-s">Back to top!</div>;
+  };
   render() {
     return (
       <div className="block mx24 my24 z5">
-        <IconButton
-          onClick={() => {
-            document.documentElement.scrollTop = 0; // fallback
-            window.scroll({
-              top: 0,
-              left: 0
-            });
+        <PopoverTrigger
+          respondsToClick={false}
+          respondsToHover={true}
+          content={this.getPopoverContent}
+          popoverProps={{
+            alignment: 'center',
+            placement: 'top',
+            padding: 'small'
           }}
-          icon="arrow-up"
-          tooltipProps={{
-            content: 'Back to top',
-            respondsToClick: false,
-            receivesFocus: false
-          }}
-          themeIcon="w36 h36 mx-auto"
-          themeButton="btn--blue w60 h60 px0 round-full shadow-darken25"
-        />
+        >
+          <Button
+            onClick={() => {
+              document.documentElement.scrollTop = 0; // fallback
+              window.scroll({
+                top: 0,
+                left: 0
+              });
+            }}
+            passthroughProps={{
+              className:
+                'btn--blue w60 h60 px0 round-full shadow-darken25 color-white flex-parent flex-parent--center-main'
+            }}
+          >
+            <Icon name="arrow-up" size={30} />
+          </Button>
+        </PopoverTrigger>
       </div>
     );
   }
