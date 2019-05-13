16e29550e09ca44e9a5ed80abd2c4ac07b82e191
Katy DeCorah <decorah@mapbox.com>
2019-05-13 16:59:12 -0400

    create helper fn

---
+++ src/helpers/title-generator.js
@@ -0,0 +1,13 @@
+export function titleGenerator(title, subsite, site) {
+  // create array for formatted title: {title} | {subsite} | {site}
+  const titleArr = [];
+  // do not push a title that is "Introduction"
+  if (title && title !== 'Introduction' && (subsite || site))
+    titleArr.push(title);
+  // push subsite, if same value doesn't exist yet, strip "for (Product)" from name
+  if (subsite && titleArr.indexOf(subsite) === -1)
+    titleArr.push(subsite.replace(/\sfor\s(iOS|Android|Vision|Unity)/, ''));
+  // push site, if same value doesn't exist yet
+  if (site && titleArr.indexOf(site) === -1) titleArr.push(site);
+  return titleArr;
+}
---
+++ tests/title-generator.test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const tagger = require('../src/helpers/title-generator.js');
+
+describe('titleGenerator', () => {
+  it('no duplicated values', () => {
+    expect(tagger.titleGenerator('iOS', 'iOS', 'iOS')).toEqual(['iOS']);
+  });
+
+  it('no duplicated values', () => {
+    expect(tagger.titleGenerator('Rerouting', 'iOS', 'iOS')).toEqual([
+      'Rerouting',
+      'iOS'
+    ]);
+  });
+
+  it('do not display subsite if it does not exist', () => {
+    expect(tagger.titleGenerator('Rerouting', 'iOS')).toEqual([
+      'Rerouting',
+      'iOS'
+    ]);
+  });
+
+  it('remove redundancy from subsite', () => {
+    expect(
+      tagger.titleGenerator('Rerouting', 'Maps SDK for iOS', 'iOS')
+    ).toEqual(['Rerouting', 'Maps SDK', 'iOS']);
+  });
+
+  it('no "Introduction" as title', () => {
+    expect(
+      tagger.titleGenerator('Introduction', 'Maps SDK for iOS', 'iOS')
+    ).toEqual(['Maps SDK', 'iOS']);
+  });
+
+  it('no "Introduction" as title', () => {
+    expect(tagger.titleGenerator('Introduction', 'iOS')).toEqual(['iOS']);
+  });
+});
