fcfa893785a76d9e363ff536b2a304293b2934a7
Katy DeCorah <decorah@mapbox.com>
2019-06-03 19:26:37 -0400

    adds web-analytics to test analytics

--- src/test-cases-app/test-cases-app.js
+++ src/test-cases-app/test-cases-app.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactTestKitchen from '@mapbox/react-test-kitchen';
 import componentIndex from './component-index'; // eslint-disable-line
+import '@mapbox/web-analytics'; // exposes global `initializeMapboxAnalytics()` function
 
 class App extends React.Component {
+  componentDidMount() {
+    if (window && window.initializeMapboxAnalytics) {
+      window.initializeMapboxAnalytics();
+    }
+  }
+
   render() {
     return <ReactTestKitchen componentIndex={componentIndex} />;
   }
