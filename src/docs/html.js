8c7b08e84ee445ca44cc92e6c58edad81647f1e9
Katy DeCorah <decorah@mapbox.com>
2019-06-04 08:28:36 -0400

    add favicon

--- src/docs/html.js
+++ src/docs/html.js
@@ -5,7 +5,7 @@ module.exports = `
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="robots" content="noindex" />
-    <title>Dr. UI</title>
+    <title>Dr. UI | Mapbox</title>
     <link
       href="https://api.mapbox.com/mapbox-assembly/mbx/v0.28.1/assembly.min.css"
       rel="stylesheet"
@@ -14,6 +14,7 @@ module.exports = `
       async
       src="https://api.mapbox.com/mapbox-assembly/mbx/v0.28.1/assembly.js"
     ></script>
+    <link rel="shortcut icon" href="https://static-assets.mapbox.com/branding/favicon/v1/favicon.ico?v=gAd4JjrGWl"/>
   </head>
   <body>
   </body>
