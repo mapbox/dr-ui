42e6d245350e18e0e339770c2a2505b1edfef3e8
Katy DeCorah <decorah@mapbox.com>
2019-07-08 14:28:25 -0400

    comments

--- src/components/feedback/forward-event.js
+++ src/components/feedback/forward-event.js
@@ -1,14 +1,27 @@
+/**
+ * A XHR-powered function that accepts your Segment event, sends it to the endpoint, and then forwards it to Segment.
+ * Events will be sent to one of two Segment projects:
+ * 1. mapbox (production) if your host is *.mapbox.com.
+ * 2. mapbox-staging otherwise.
+ *
+ * @param {event} - A Segment event object
+ * @param {webhook} - Object containing the production and staging webhooks {production: '<webhook url>', 'staging: '<webhook url>'}
+ *
+ */
 export default function forwardEvent(event, webhook, callback) {
   callback = callback || function() {};
 
+  // window and event must be present to make the proper request
   if (typeof window === 'undefined') return;
   if (!event) {
     throw new Error('event argument required');
   }
-
+  // checks if the current page is on production or staging
+  // then determines which webhook to post to
   const isProduction = /(^|\S+\.)mapbox\.com/.test(window.location.host);
   const url = isProduction ? webhook.production : webhook.staging;
 
+  // builds the xhr request to post the Segment event to the webhook
   const xhr = new XMLHttpRequest();
   xhr.open('POST', url);
   xhr.setRequestHeader('Accept', 'application/json');
@@ -17,6 +30,7 @@ export default function forwardEvent(event, webhook, callback) {
   xhr.onload = handleLoad;
   xhr.send(JSON.stringify(event));
 
+  // handles posting the Segment event to the webhook URL
   function handleLoad() {
     if (xhr.status === 200) {
       callback();
@@ -37,6 +51,7 @@ export default function forwardEvent(event, webhook, callback) {
     handleError(error);
   }
 
+  // handles xhr error
   function handleError(error) {
     callback(error);
   }
