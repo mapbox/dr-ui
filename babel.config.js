32a12f49df08004488f030af126ba08a9dcfcbac
Katy DeCorah <decorah@mapbox.com>
2019-08-13 16:59:35 -0400

    [babel] Add babel config to helpers and plugins, add eslint plugins (#164)
    * make babel process plugins and helpers
    
    * make babel transform arrow functions and object assign
    
    * satisfy eslint warning about react version
    
    * clean up
    
    * Update package.json
    
    * Revert aeed83b7614dd8e1bd8b00892126859a9cb2246d
    
    * use eslint-plugin-es: forbid features that babel isn't compiling
    
    * add eslint-plugin-compat to double check compatibility based on browserslist in package.json
    
    * Update CHANGELOG.md

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,9 @@
 
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: ['@babel/plugin-proposal-class-properties']
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-arrow-functions',
+    '@babel/plugin-transform-object-assign'
+  ]
 };
