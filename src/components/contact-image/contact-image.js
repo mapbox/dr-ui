5d565eca1d8dbc2974efa9bbc9a28bc7701f8c49
Katy DeCorah <decorah@mapbox.com>
2019-08-14 09:57:32 -0400

    compress svg

--- src/components/contact-image/contact-image.js
+++ src/components/contact-image/contact-image.js
@@ -8,52 +8,52 @@ export default class ContactImage extends React.Component {
       <svg width={props.size} height={props.size} viewBox="0 0 108 108">
         <circle cx="53" cy="54" r="45" fill="#ada0ea" />
         <rect
-          x="25.7418"
-          y="44.8515"
-          width="31.0206"
-          height="28.5389"
-          rx="8.6736"
-          ry="8.6736"
-          transform="translate(0.6265 -0.4322) rotate(0.6049)"
+          x="25.742"
+          y="44.852"
+          width="31.021"
+          height="28.539"
+          rx="8.674"
+          ry="8.674"
+          transform="rotate(.605 41.25 59.125)"
           fill="#9179e8"
         />
-        <polygon
-          points="29.114 79.175 55.156 56 29 55.724 29.114 79.175"
+        <path
           fill="#9179e8"
+          d="M29.114 79.175L55.156 56 29 55.724l.114 23.451z"
         />
         <rect
-          x="34.5315"
-          y="32.6076"
-          width="44.6696"
-          height="33.5022"
-          rx="8.6082"
-          ry="8.6082"
-          transform="translate(0.5243 -0.5976) rotate(0.6049)"
+          x="34.532"
+          y="32.608"
+          width="44.67"
+          height="33.502"
+          rx="8.608"
+          ry="8.608"
+          transform="rotate(.605 56.866 49.362)"
           fill="#784def"
         />
-        <polygon
-          points="69.805 75.432 39.181 47 70.534 47.331 69.805 75.432"
+        <path
           fill="#784def"
+          d="M69.805 75.432L39.181 47l31.353.331-.729 28.101z"
         />
         <circle
           cx="45"
           cy="49"
           r="3"
-          transform="translate(-4.4724 93.4802) rotate(-89.3951)"
+          transform="rotate(-89.395 45 49)"
           fill="#fff"
         />
         <circle
           cx="56"
           cy="49"
           r="3"
-          transform="translate(6.4115 104.4796) rotate(-89.3951)"
+          transform="rotate(-89.395 56 49)"
           fill="#fff"
         />
         <circle
           cx="67"
           cy="49"
           r="3"
-          transform="translate(17.2954 115.4789) rotate(-89.3951)"
+          transform="rotate(-89.395 67 49)"
           fill="#fff"
         />
       </svg>
--- src/components/warning-image/warning-image.js
+++ src/components/warning-image/warning-image.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 export default class WarningImage extends React.Component {
   render() {
     const { props } = this;
-
     const fill = {
       red: { bg: '#ffb9a9', icon: '#d85a3d' },
       orange: { bg: '#fbcea6', icon: '#ba7334' }
@@ -19,9 +18,7 @@ export default class WarningImage extends React.Component {
         <circle fill={fill[this.props.color].bg} cx="30" cy="30" r="25" />
         <path
           fill={fill[this.props.color].icon}
-          d="M30,15.7c-1.3,0-2.7,0.7-3.3,1.8l-9.6,20.4c-1.1,1.8,0,4.4,2.4,4.4H30h10.4c2.4,0,3.6-2.7,2.4-4.4l-9.6-20.4
-      	C32.7,16.3,31.3,15.7,30,15.7z M30,22.3c1.3,0,2.2,0.9,2.2,2.2V29c0,1.3-0.9,2.2-2.2,2.2c-1.3,0-2.2-0.9-2.2-2.2v-4.4
-      	C27.8,23.2,28.7,22.3,30,22.3z M30,33.4c1.3,0,2.2,0.9,2.2,2.2s-0.9,2.2-2.2,2.2c-1.3,0-2.2-0.9-2.2-2.2S28.7,33.4,30,33.4z"
+          d="M30 15.7c-1.3 0-2.7.7-3.3 1.8l-9.6 20.4c-1.1 1.8 0 4.4 2.4 4.4h20.9c2.4 0 3.6-2.7 2.4-4.4l-9.6-20.4c-.5-1.2-1.9-1.8-3.2-1.8zm0 6.6c1.3 0 2.2.9 2.2 2.2V29c0 1.3-.9 2.2-2.2 2.2-1.3 0-2.2-.9-2.2-2.2v-4.4c0-1.4.9-2.3 2.2-2.3zm0 11.1c1.3 0 2.2.9 2.2 2.2s-.9 2.2-2.2 2.2c-1.3 0-2.2-.9-2.2-2.2s.9-2.2 2.2-2.2z"
         />
       </svg>
     );
