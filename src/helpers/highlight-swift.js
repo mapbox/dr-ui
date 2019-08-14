3e5b386f2d03e1be06eb86070661cb94a365fbc9
Colleen <colleen.j.mcginnis@gmail.com>
2019-08-14 10:08:20 -0700

    add syntax highlighting functions

---
+++ src/helpers/highlight-swift.js
@@ -0,0 +1,8 @@
+import Prism from 'prismjs';
+import 'prismjs/components/prism-swift';
+
+function highlightSwift(rawCode) {
+  return rawCode ? Prism.highlight(rawCode, Prism.languages['swift']) : '';
+}
+
+export { highlightSwift };
