f0b7e2b3deba8aeab5e98c42c194bfe493e4ae19
Colleen <colleen.j.mcginnis@gmail.com>
2019-08-29 09:51:38 -0700

    address @katydecorah's comments

--- src/components/video/video.js
+++ src/components/video/video.js
@@ -2,12 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class Video extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { reducedMotion: false };
+  }
+
+  componentDidMount() {
+    const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+    if (mediaQuery.matches) this.setState({ reducedMotion: true });
+  }
+
   render() {
     return (
       <div>
         <video
-          autoPlay
-          loop
+          autoPlay={!this.state.reducedMotion}
+          loop={!this.state.reducedMotion}
+          controls={this.state.reducedMotion}
           muted
           width="100%"
           className="block mx-auto"
@@ -15,7 +26,10 @@ export default class Video extends React.Component {
           type="video/mp4"
           title={this.props.title}
         >
-          Your browser doesn't support embedded videos.
+          <p>
+            Your browser doesn't support HTML5 video. Here is a{' '}
+            <a href={this.props.src}>link to the video</a> instead.
+          </p>
         </video>
       </div>
     );
