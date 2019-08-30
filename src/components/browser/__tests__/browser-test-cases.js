09b13953578cd78d1e1e67eb92c7c3c241eff4cb
Katy DeCorah <decorah@mapbox.com>
2019-08-30 09:47:40 -0400

    adds example image and video for browser-test-cases

--- src/components/browser/__tests__/browser-test-cases.js
+++ src/components/browser/__tests__/browser-test-cases.js
@@ -9,7 +9,7 @@ testCases.basic = {
   props: {
     children: (
       <img
-        src="https://docs.mapbox.com/studio-manual/assets/ref-styles-manage-styles-1200-599fd585f5f2d8e0729d5fe37f9722c0.webp"
+        src="./assets/browser-example.png"
         className="round-b"
         alt="manage-styles"
       />
@@ -28,7 +28,7 @@ testCases.video = {
         muted
         width="100%"
         className="block mx-auto round-b"
-        src="https://docs.mapbox.com/studio-manual/assets/filter-source-layer-8794aabd77c86712e6aee99bb764182e.mp4"
+        src="./assets/browser-example.mp4"
         type="video/mp4"
         title="animated GIF walking through how to filter a source layer by adding a vector source clicking Filter > Create filter > select a data field from the list provided > select a value from the list provided"
       >
