65588e70fa597c52a1c520928682b59b8ca60e15
Colleen <colleen.j.mcginnis@gmail.com>
2019-09-10 17:01:50 -0700

    generate snapshots, fix tests

--- src/components/toggleable-code-block/__tests__/toggleable-code-block.test.js
+++ src/components/toggleable-code-block/__tests__/toggleable-code-block.test.js
@@ -20,40 +20,4 @@ describe('toggleable-code-block', () => {
       expect(tree).toMatchSnapshot();
     });
   });
-
-  describe(testCases.objc.description, () => {
-    let testCase;
-    let wrapper;
-    let tree;
-
-    beforeEach(() => {
-      testCase = testCases.objc;
-      wrapper = renderer.create(
-        React.createElement(testCase.component, testCase.props)
-      );
-      tree = wrapper.toJSON();
-    });
-
-    test('renders as expected', () => {
-      expect(tree).toMatchSnapshot();
-    });
-  });
-
-  describe(testCases.java.description, () => {
-    let testCase;
-    let wrapper;
-    let tree;
-
-    beforeEach(() => {
-      testCase = testCases.java;
-      wrapper = renderer.create(
-        React.createElement(testCase.component, testCase.props)
-      );
-      tree = wrapper.toJSON();
-    });
-
-    test('renders as expected', () => {
-      expect(tree).toMatchSnapshot();
-    });
-  });
 });
