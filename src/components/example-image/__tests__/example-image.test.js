7a9681bfe8c68e7ebf3714fa1a6cedf66c4fae2e
Colleen McGinnis <colleen.mcginnis@mapbox.com>
2019-12-23 08:39:03 -0800

    Add new related page component (#213)
    * add new related page component
    
    * design suggestions for RelatedPage (#214)
    
    * design suggestions for related-page
    
    * clean up
    
    * add example and glossary images
    
    * update changelog
    
    * more changelog updates
    
    Co-authored-by: Katy DeCorah <katydecorah@gmail.com>

---
+++ src/components/example-image/__tests__/example-image.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { testCases } from './example-image-test-cases.js';
+
+describe('example-image', () => {
+  describe(testCases.basic.description, () => {
+    let testCase;
+    let wrapper;
+    let tree;
+
+    beforeEach(() => {
+      testCase = testCases.basic;
+      wrapper = renderer.create(
+        React.createElement(testCase.component, testCase.props)
+      );
+      tree = wrapper.toJSON();
+    });
+
+    test('renders as expected', () => {
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  describe(testCases.bigger.description, () => {
+    let testCase;
+    let wrapper;
+    let tree;
+
+    beforeEach(() => {
+      testCase = testCases.bigger;
+      wrapper = renderer.create(
+        React.createElement(testCase.component, testCase.props)
+      );
+      tree = wrapper.toJSON();
+    });
+
+    test('renders as expected', () => {
+      expect(tree).toMatchSnapshot();
+    });
+  });
+});
