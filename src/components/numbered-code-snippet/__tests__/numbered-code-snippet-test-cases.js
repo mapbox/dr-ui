bbc2afee83d87cbed989594f668d4785535cd651
Katy DeCorah <decorah@mapbox.com>
2020-01-03 08:55:48 -0500

    create highlight/theme-css

--- src/components/numbered-code-snippet/__tests__/numbered-code-snippet-test-cases.js
+++ src/components/numbered-code-snippet/__tests__/numbered-code-snippet-test-cases.js
@@ -1,5 +1,6 @@
 import NumberedCodeSnippet from '../numbered-code-snippet';
 import { highlightSwift } from '../../highlight/swift';
+import { highlightThemeCss } from '../../highlight/theme-css.js';
 
 const testCases = {};
 
@@ -46,124 +47,6 @@ class DDSCircleLayerTutorialViewController: UIViewController, MGLMapViewDelegate
     }
 }`;
 
-let highlightTheme = `
-code[class*='language-'],
-pre[class*='language-'] {
-  color: #273d56;
-  background: none;
-  font-family: Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace;
-  text-align: left;
-  white-space: pre;
-  word-spacing: normal;
-  word-break: normal;
-  word-wrap: normal;
-  line-height: 1.5;
-
-  -moz-tab-size: 4;
-  -o-tab-size: 4;
-  tab-size: 4;
-
-  -webkit-hyphens: none;
-  -moz-hyphens: none;
-  -ms-hyphens: none;
-  hyphens: none;
-}
-
-/* Code blocks */
-pre[class*='language-'] {
-  padding: 1em;
-  margin: 0.5em 0;
-  overflow: auto;
-  border-radius: 0.3em;
-}
-
-:not(pre) > code[class*='language-'],
-pre[class*='language-'] {
-  background: #272822;
-}
-
-/* Inline code */
-:not(pre) > code[class*='language-'] {
-  padding: 0.1em;
-  border-radius: 0.3em;
-  white-space: normal;
-}
-
-.token.comment,
-.token.prolog,
-.token.doctype,
-.token.cdata {
-  color: #53708e;
-}
-
-.token.punctuation {
-  color: #273d56;
-}
-
-.namespace {
-  opacity: 0.7;
-}
-
-.token.property,
-.token.tag,
-.token.constant,
-.token.symbol,
-.token.deleted {
-  color: #314ccd;
-}
-
-.token.boolean,
-.token.number {
-  color: #7753eb;
-}
-
-.token.selector,
-.token.attr-name,
-.token.string,
-.token.char,
-.token.builtin,
-.token.inserted {
-  color: #ce2c69;
-}
-
-.token.operator,
-.token.entity,
-.token.url,
-.language-css .token.string,
-.style .token.string,
-.token.variable {
-  color: #273d56;
-}
-
-.token.atrule,
-.token.attr-value,
-.token.function,
-.token.class-name {
-  color: #4264fb;
-}
-
-.token.keyword {
-  color: #314ccd;
-}
-
-.token.regex,
-.token.important {
-  color: #fd971f;
-}
-
-.token.important,
-.token.bold {
-  font-weight: bold;
-}
-.token.italic {
-  font-style: italic;
-}
-
-.token.entity {
-  cursor: help;
-}
-`;
-
 testCases.basic = {
   component: NumberedCodeSnippet,
   description: 'Basic',
@@ -171,7 +54,7 @@ testCases.basic = {
     code: code,
     highlightedCode: highlightSwift(code),
     maxHeight: 450,
-    highlightThemeCss: highlightTheme,
+    highlightThemeCss: highlightThemeCss,
     copyRanges: [[6, 17]],
     onCopy: () => {}
   }
@@ -185,7 +68,7 @@ testCases.noCollapse = {
     code: code,
     highlightedCode: highlightSwift(code),
     maxHeight: 450,
-    highlightThemeCss: highlightTheme,
+    highlightThemeCss: highlightThemeCss,
     copyRanges: [[21, 23], [38, 38]],
     onCopy: () => {}
   }
