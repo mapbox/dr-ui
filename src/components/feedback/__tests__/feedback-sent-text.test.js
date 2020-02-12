30ecfe312bc7ef50fd0c83d4d500222f30170a48
Katy DeCorah <decorah@mapbox.com>
2020-02-12 14:42:16 -0500

    adds snapshot tests for each Feedback component interaction (#251)
    * adds snapshot tests for each feedback interaction (sent helpful rating and sent text feedback)
    
    * clean up

---
+++ src/components/feedback/__tests__/feedback-sent-text.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Feedback from '..';
+import toJson from 'enzyme-to-json';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+describe('Sent text feedback', () => {
+  const feedback = shallow(
+    <Feedback
+      site="dr-ui"
+      location={{
+        pathname: '/mapbox-gl-js/api/',
+        hash: '#lnglat'
+      }}
+    />
+  );
+  feedback.setState({
+    helpful: true,
+    feedback: 'Please add an example',
+    feedbackSent: true
+  });
+
+  test('basic', () => {
+    expect(toJson(feedback)).toMatchSnapshot();
+  });
+});
