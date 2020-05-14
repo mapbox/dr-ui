7ec15042644922039bbb79334eec2a531739d08f
Katy DeCorah <decorah@mapbox.com>
2020-05-14 15:14:14 -0400

    more spacing

--- src/components/product-menu/product-menu.js
+++ src/components/product-menu/product-menu.js
@@ -18,7 +18,7 @@ class ProductMenu extends React.PureComponent {
         : undefined
     };
     return (
-      <span className="flex-child ml3">
+      <span className="flex-child ml6">
         <Tag {...tagProps} />
       </span>
     );
