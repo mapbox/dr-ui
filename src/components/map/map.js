a5ab890e476ea8b3cacb6e10f59229a2a5cd27d2
Katy DeCorah <decorah@mapbox.com>
2020-06-03 10:12:25 -0400

    reorder

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -20,12 +20,13 @@ class Map extends React.PureComponent {
     this.mapContainer = React.createRef();
   }
 
-  // turn off loader and set the token
-  setToken = token => {
-    this.setState({ loading: false }, () => {
-      mapboxgl.accessToken = token;
-      this.initMap();
-    });
+  // wait for GLWrapper to let us know if we can load the map
+  loadMap = status => {
+    // if loadMap returns true, we can load the map
+    if (status) {
+      // make sure there is an accessToken prop or a token from MapboxPageShell
+      this.handleToken();
+    }
   };
 
   handleToken = () => {
@@ -43,13 +44,13 @@ class Map extends React.PureComponent {
     }
   };
 
-  // wait for GLWrapper to let us know if we can load the map
-  loadMap = status => {
-    // if loadMap returns true, we can load the map
-    if (status) {
-      // make sure there is an accessToken prop or a token from MapboxPageShell
-      this.handleToken();
-    }
+  // turn off loader and set the token
+  setToken = token => {
+    this.setState({ loading: false }, () => {
+      mapboxgl.accessToken = token;
+      // once we have the token, we can load the map
+      this.initMap();
+    });
   };
 
   initMap() {
