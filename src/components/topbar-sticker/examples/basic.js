d431e7e2682117e2d80b735c6b059edd7b73fdd9
Katy DeCorah <decorah@mapbox.com>
2020-06-09 15:19:24 -0400

    TopbarSticker

--- src/components/topbar-sticker/examples/basic.js
+++ src/components/topbar-sticker/examples/basic.js
@@ -3,15 +3,28 @@ Basic.
 */
 import React from 'react';
 import TopbarSticker from '../topbar-sticker';
+import ProductMenu from '../../product-menu/product-menu';
+import Search from '../../search/search';
 
 export default class Example extends React.Component {
   render() {
     return (
-      <div style={{ background: 'pink', height: 3000 }}>
+      <div className="h300">
         <div className="px24 py12">Above the bar.</div>
         <TopbarSticker>
-          <div className="px24 py12">
-            Here's some content that sticks to the top.
+          <div className="limiter">
+            <div className="grid">
+              <div className="col col--4-mm col--12">
+                <div className="ml24-mm pt12" style={{ height: 52 }}>
+                  <ProductMenu productName="Dr. UI" homePage="/dr-ui/" />
+                </div>
+              </div>
+              <div className="col col--8-mm col--12">
+                <div className="flex-parent-mm flex-parent--center-cross flex-parent--end-main h-full-mm wmax300 wmax-full-mm mb0-mm mb12">
+                  <Search site="dr-ui" />
+                </div>
+              </div>
+            </div>
           </div>
         </TopbarSticker>
         <div className="px24 py12">
