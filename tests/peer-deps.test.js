0fc9bc9ea4b9740324466554c0a1473494605646
Katy DeCorah <decorah@mapbox.com>
2020-06-14 12:53:14 -0400

    Update react, react-dom, prop-types, react-test-renderer; Make react and react-dom peerDeps

--- tests/peer-deps.test.js
+++ tests/peer-deps.test.js
@@ -1,12 +1,26 @@
 'use strict';
 
-const { dependencies, peerDependencies } = require('../package.json');
+const { dependencies, peerDependencies, devDependencies } = require('../package.json');
 
 describe('peerDependencies are up-to-date', () => {
   test(`@sentry/browser`, () => {
     expect(
       dependencies['@sentry/browser'] ===
         `^${peerDependencies['@sentry/browser']}`
+    ).toBeTruthy();
+  });
+
+  test(`react`, () => {
+    expect(
+      devDependencies['react'] ===
+        peerDependencies['react']
+    ).toBeTruthy();
+  });
+
+  test(`react-dom`, () => {
+    expect(
+      devDependencies['react-dom'] ===
+        peerDependencies['react-dom']
     ).toBeTruthy();
   });
 });
