322f28a497c452cdd51aff9e5cfc1b5fa9a32bec
Katy DeCorah <decorah@mapbox.com>
2020-06-14 13:22:14 -0400

    Update peer-deps.test.js

--- tests/peer-deps.test.js
+++ tests/peer-deps.test.js
@@ -1,6 +1,10 @@
 'use strict';
 
-const { dependencies, peerDependencies, devDependencies } = require('../package.json');
+const {
+  dependencies,
+  peerDependencies,
+  devDependencies
+} = require('../package.json');
 
 describe('peerDependencies are up-to-date', () => {
   test(`@sentry/browser`, () => {
@@ -11,23 +15,18 @@ describe('peerDependencies are up-to-date', () => {
   });
 
   test(`react`, () => {
-    expect(
-      devDependencies['react'] ===
-        peerDependencies['react']
-    ).toBeTruthy();
+    expect(devDependencies['react'] === peerDependencies['react']).toBeTruthy();
   });
 
   test(`react-dom`, () => {
     expect(
-      devDependencies['react-dom'] ===
-        peerDependencies['react-dom']
+      devDependencies['react-dom'] === peerDependencies['react-dom']
     ).toBeTruthy();
   });
 
   test(`@mapbox/mr-ui`, () => {
     expect(
-      dependencies['@mapbox/mr-ui'] ===
-        peerDependencies['@mapbox/mr-ui']
+      dependencies['@mapbox/mr-ui'] === peerDependencies['@mapbox/mr-ui']
     ).toBeTruthy();
   });
 
