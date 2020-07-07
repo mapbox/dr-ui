b8ad51e7508e2d71c01e151abc0e486be0976d28
Katy DeCorah <decorah@mapbox.com>
2020-07-07 14:13:50 -0400

    Add AnalyticsShell component

---
+++ src/components/analytics-shell/__tests__/analytics-shell.test.js
@@ -0,0 +1,20 @@
+import renderer from 'react-test-renderer';
+import { testCases } from './analytics-shell-test-cases.js';
+
+describe('AnalyticsShell', () => {
+  describe(testCases.basic.description, () => {
+    let testCase;
+    let wrapper;
+    let tree;
+
+    beforeEach(() => {
+      testCase = testCases.basic;
+      wrapper = renderer.create(testCase.element);
+      tree = wrapper.toJSON();
+    });
+
+    test('renders as expected', () => {
+      expect(tree).toMatchSnapshot();
+    });
+  });
+});
---
+++ src/components/analytics-shell/analytics-shell.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import PropTypes from 'prop-types';
+import * as Sentry from '@sentry/browser';
+
+const environment =
+  typeof window !== 'undefined'
+    ? /(^|\S+\.)mapbox\.com/.test(window.location.host)
+      ? 'production'
+      : 'staging'
+    : undefined;
+
+export default class AnalyticsShell extends React.Component {
+  componentDidMount() {
+    const {
+      disableWebAnalytics,
+      disableSentry,
+      webAnalytics,
+      sentry
+    } = this.props;
+    // initialize analytics
+    if (window && window.initializeMapboxAnalytics && !disableWebAnalytics) {
+      window.initializeMapboxAnalytics({
+        ...webAnalytics
+      });
+    }
+    // initialize Sentry
+    if (!disableSentry) {
+      Sentry.init({
+        ...sentry,
+        environment
+      });
+    }
+  }
+
+  render() {
+    const { location, children, domain } = this.props;
+    return (
+      <React.Fragment>
+        <Helmet>
+          <link rel="canonical" href={`${domain}${location.pathname}`} />
+        </Helmet>
+        {children}
+      </React.Fragment>
+    );
+  }
+}
+
+AnalyticsShell.defaultProps = {
+  // production website domain
+  domain: 'https://docs.mapbox.com',
+  // do no disable sentry by default
+  disableSentry: false,
+  // default Sentry options, docs-subdomain project
+  sentry: {
+    dsn: 'https://6ba8cfeeedad4fb7acb8576f0fd6e266@sentry.io/1384508'
+  },
+  // do not disable web-analytics by default
+  disableWebAnalytics: false,
+  // default web-analytics options, disable Drift by default
+  webAnalytics: {
+    segmentIntegrations: {
+      Drift: false
+    }
+  }
+};
+
+AnalyticsShell.propTypes = {
+  children: PropTypes.node.isRequired,
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired
+  }).isRequired,
+  domain: PropTypes.string,
+  sentry: PropTypes.shape({
+    // Customize Sentry options: https://docs.sentry.io/error-reporting/configuration/?platform=browser
+    dsn: PropTypes.string
+  }),
+  webAnalytics: PropTypes.object, // Customize web-analytics options: https://github.com/mapbox/web-analytics
+  disableSentry: PropTypes.bool, // If true, Sentry will not initialize.
+  disableWebAnalytics: PropTypes.bool // If true, Mapbox analytics (initializeMapboxAnalytics) will not initialize.
+};
