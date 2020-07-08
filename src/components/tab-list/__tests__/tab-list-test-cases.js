820a860743f6d5127799794b8363baee21271775
Katy DeCorah <decorah@mapbox.com>
2020-07-08 15:30:42 -0400

    Add mr-ui/tab-list

---
+++ src/components/tab-list/__tests__/tab-list-test-cases.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import safeSpy from '../../../test-utils/safe-spy';
+import TabList from '../tab-list';
+import Icon from '@mapbox/mr-ui/icon';
+
+const testCases = {};
+
+testCases.basic = {
+  description: 'basic',
+  component: TabList,
+  props: {
+    onChange: safeSpy(),
+    activeItem: 'three',
+    items: [
+      { id: 'one', label: 'Label one' },
+      { id: 'two', label: 'Label two' },
+      { id: 'three', label: 'Label three' },
+      { id: 'four', label: 'Label four' }
+    ]
+  }
+};
+
+testCases.links = {
+  description: 'links',
+  component: TabList,
+  props: {
+    activeItem: 'one',
+    items: [
+      { id: 'one', label: 'Label one', href: '#' },
+      { id: 'two', label: 'Label two', href: '#' },
+      { id: 'three', label: 'Label three', href: '#' },
+      { id: 'four', label: 'Label four', href: '#' }
+    ]
+  }
+};
+
+testCases.truncateAll = {
+  description: 'truncate all',
+  component: TabList,
+  props: {
+    activeItem: 'one',
+    truncateBy: 0,
+    items: [
+      { id: 'one', label: 'Label one', href: '#' },
+      { id: 'two', label: 'Label two', href: '#' },
+      { id: 'three', label: 'Label three', href: '#' },
+      { id: 'four', label: 'Label four', href: '#' }
+    ]
+  }
+};
+
+testCases.labelNode = {
+  description: 'a label contains a node',
+  component: TabList,
+  props: {
+    onChange: safeSpy(),
+    activeItem: 'three',
+    items: [
+      { id: 'one', label: <Icon name="star" /> },
+      { id: 'two', label: 'Label two' },
+      { id: 'three', label: 'Label three' },
+      { id: 'four', label: 'Label four' }
+    ]
+  }
+};
+
+export { testCases };
