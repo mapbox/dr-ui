3ea3d8fe64a6d85d8e36009a042ba50592726eef
Katy DeCorah <decorah@mapbox.com>
2020-07-13 09:04:22 -0400

    Revert changes to PageLayout (will happen in another PR)

--- src/components/page-layout/__tests__/page-layout-test-cases.js
+++ src/components/page-layout/__tests__/page-layout-test-cases.js
@@ -324,22 +324,4 @@ testCases.code = {
   }
 };
 
-const BadFunction = () => {};
-
-testCases.oops = {
-  description: 'Trigger error boundary in content',
-  component: PageLayout,
-  props: {
-    sidebarContent: <div>Some content</div>,
-    sidebarTitle: 'Some title',
-    sidebarContentStickyTop: 0,
-    sidebarContentStickyTopNarrow: 0,
-    children: (
-      <div>
-        <BadFunction /> Doc content
-      </div>
-    )
-  }
-};
-
 export { testCases };
