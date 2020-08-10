73e8d66c9b16e75c6f4edda039845e2a22a89a38
Katy DeCorah <decorah@mapbox.com>
2020-08-10 15:32:51 -0400

    Documentation

--- src/components/page-layout/page-layout.js
+++ src/components/page-layout/page-layout.js
@@ -113,16 +113,14 @@ export default class PageLayout extends React.Component {
 
 PageLayout.propTypes = {
   children: PropTypes.node,
-  /** Provided by Batfish, the `pathnam` (relative url) of the current page is required */
+  /** Provided by Batfish, the `pathname` (relative url) of the current page is required */
   location: PropTypes.shape({
     pathname: PropTypes.string.isRequired
   }).isRequired,
   /**
-Define the layout:
-- `layout` - one of: `page`, `accordion`, `example`, `full`, `exampleIndex`.
-
-Field | Description | Conditions
+`frontMatter` prop | Description | Conditions
 ---|---|---
+`layout` | One of: `page`, `accordion`, `example`, `full`, `exampleIndex`. |
 `navOrder` | If defined with a number, the page will be added as a top level navigation item. This is the canonical way for defining a top level page. |
 `hideTitle` | Hide the title of the page. |
 `hideFeedback` | Remove the feedback component from the bottom of the page. |
