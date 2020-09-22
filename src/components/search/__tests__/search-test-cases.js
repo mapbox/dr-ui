e7358b3dbd78e76a9eea5694ed521a2d21f5322b
Katy DeCorah <decorah@mapbox.com>
2020-09-22 10:13:05 -0400

    Append note to each test case description to remind us to be mindful of query usage

--- src/components/search/__tests__/search-test-cases.js
+++ src/components/search/__tests__/search-test-cases.js
@@ -6,23 +6,23 @@ import DisableModal from '../examples/disabled';
 import Narrow from '../examples/narrow';
 import Results from './results';
 
-const testCases = {};
+let testCases = {};
 
 testCases.basic = {
-  description: 'Basic search',
+  description: 'Basic search.',
   element: <Basic />
 };
 
 testCases.site = {
   component: Search,
-  description: 'Basic search with `site` set to show filter toggle',
+  description: 'Basic search with `site` set to show filter toggle.',
   props: {
     site: 'API'
   }
 };
 
 testCases.dark = {
-  description: 'Search with dark background, custom placeholder',
+  description: 'Search with dark background, custom placeholder.',
   element: (
     <div className="py24 px24 bg-gray-dark">
       <div className="w-full">
@@ -33,18 +33,18 @@ testCases.dark = {
 };
 
 testCases.disableModal = {
-  description: 'Search with `disableModal` option set',
+  description: 'Search with `disableModal` option set.',
   element: <DisableModal />
 };
 
 testCases.narrow = {
-  description: 'Search with `narrow` option set',
+  description: 'Search with `narrow` option set.',
   element: <Narrow />
 };
 
 testCases.withConnector = {
   component: Search,
-  description: 'Search with custom connector',
+  description: 'Search with custom connector.',
   props: {
     inputId: 'search5',
     connector: new SiteSearchAPIConnector({
@@ -61,5 +61,13 @@ testCases.resultsOnly = {
     'Set `resultsOnly: true` to true. Select an item to see search results, with a custom message when there are no search results for a particular query.',
   element: <Results />
 };
+
+testCases = Object.keys(testCases).reduce((obj, item) => {
+  obj[item] = {
+    ...testCases[item],
+    description: `${testCases[item].description} Open https://app.swiftype.com/engines/docs/analytics/live while developing to watch query usage.`
+  };
+  return obj;
+}, {});
 
 export { testCases };
