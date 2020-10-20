e57e681b466ad28266bac3dca0b2ab44f74c5e0c
Katy DeCorah <decorah@mapbox.com>
2020-10-20 10:19:40 -0400

    Fix document

--- src/components/on-this-page/on-this-page.js
+++ src/components/on-this-page/on-this-page.js
@@ -10,7 +10,8 @@ import classnames from 'classnames';
 import debounce from 'debounce';
 
 // check if client body width is >= 1200
-const isMXL = document.body.clientWidth >= 1200;
+const isMXL =
+  typeof document !== 'undefined' ? document.body.clientWidth >= 1200 : false;
 
 export default class OnThisPage extends React.PureComponent {
   constructor(props) {
