d62df9eb3ad5d8d604daa74999c0751b354b9d25
danswick <dan.swick@gmail.com>
2020-10-30 19:16:26 -0700

    remove sidbar configs

--- src/components/page-layout/layout.config.js
+++ src/components/page-layout/layout.config.js
@@ -3,25 +3,19 @@ export default {
     sidebar: 'toc', // heading table of contents
     sidebarTheme: '' // blank sidebar background
   },
-  accordion: {
-    sidebar: 'accordion' // NavigationAccordion sidebar
-  },
   example: {
     aside: 'none', // do not show aside
-    sidebar: 'sectioned', // SectionedNavigation sidebar
     hideSubItems: false, // show headings and subitems in sitebar
     includeFilterBar: false // hide filter bar
   },
   exampleIndex: {
     aside: 'none', // do not show aside
-    sidebar: 'sectioned', // SectionedNavigation sidebar
     hideSubItems: true, // only show sidebar headings
     showCards: true, // show example cards
     hideFeedback: true, // hide feedback module
     includeFilterBar: false // hide filter bar
   },
   full: {
-    aside: 'none', // do not show aside
-    sidebar: 'none' // no sidebar
+    aside: 'none' // do not show aside
   }
 };
