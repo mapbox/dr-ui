77767c1830da92b5aae1d774f441de7d3454168b
Katy DeCorah <decorah@mapbox.com>
2020-11-06 16:14:38 -0500

    if there is only one navTab, hide the NavigationAccordion on mobile; combine function

--- src/components/page-layout/components/sidebar.js
+++ src/components/page-layout/components/sidebar.js
@@ -2,33 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import NavigationAccordion from '../../navigation-accordion';
 import ProductMenu from '../../product-menu/product-menu';
+import classnames from 'classnames';
 
 export default class Sidebar extends React.Component {
-  getSidebarContent = () => {
+  render() {
     const {
-      location,
-      navigation,
-      children,
       constants,
+      navigation,
+      location,
+      children,
       parentPath
     } = this.props;
-    return (
-      <NavigationAccordion
-        constants={constants}
-        navigation={navigation.navTabs}
-        location={location}
-        parentPage={parentPath}
-      >
-        {children}
-      </NavigationAccordion>
-    );
-  };
-
-  render() {
-    const { constants, navigation } = this.props;
     const { SITE, BASEURL } = constants;
-    const { title, tag, path } = navigation;
-
+    const { title, tag, path, navTabs } = navigation;
     return (
       <div
         data-swiftype-index="false"
@@ -43,7 +29,21 @@ export default class Sidebar extends React.Component {
             homePage={`${BASEURL}/${path || ''}`}
           />
         </div>
-        {this.getSidebarContent()}
+        <div
+          className={classnames('', {
+            // if there is only one navTab, hide the NavigationAccordion on mobile
+            'none block-mm': navigation && navTabs && navTabs.length < 2
+          })}
+        >
+          <NavigationAccordion
+            constants={constants}
+            navigation={navTabs}
+            location={location}
+            parentPage={parentPath}
+          >
+            {children}
+          </NavigationAccordion>
+        </div>
       </div>
     );
   }
