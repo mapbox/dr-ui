20b88bb19f2122d3112e709199465ca1c6db78fe
Katy DeCorah <decorah@mapbox.com>
2020-11-18 09:46:30 -0500

    clean up

--- src/components/on-this-page/helpers.js
+++ src/components/on-this-page/helpers.js
@@ -45,10 +45,15 @@ export function getActiveHeaderAnchor(topOffset) {
   // if there are no found header anchors, return undefined
   if (!headersAnchors || headersAnchors.length === 0) return undefined;
 
-  const firstAnchorUnderViewportTop = headersAnchors.filter((anchor) => {
+  const anchorsUnderViewportTop = headersAnchors.filter((anchor) => {
     const { top } = anchor.getBoundingClientRect();
     if (top >= topOffset) return anchor;
-  })[0];
+  });
+
+  const firstAnchorUnderViewportTop =
+    anchorsUnderViewportTop && anchorsUnderViewportTop.length
+      ? anchorsUnderViewportTop[0]
+      : undefined;
 
   const lastAnchor = headersAnchors[headersAnchors.length - 1];
 
