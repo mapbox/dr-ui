df0ad716f0902178652e594798cdc6e54f4dc382
Katy DeCorah <decorah@mapbox.com>
2021-01-19 13:07:18 -0500

    The `vimeoThumbnail` prop in `RelatedPage` no longer accepts a string

--- src/components/related-page/examples/video.js
+++ src/components/related-page/examples/video.js
@@ -3,6 +3,14 @@ Video.
 */
 import React from 'react';
 import RelatedPage from '../related-page';
+import { scopeAppropriateImage } from '@mapbox/appropriate-images-react';
+
+const AppropriateImage = scopeAppropriateImage({
+  'vimeo-thumb': {
+    basename: 'vimeo-thumb.jpg',
+    sizes: [{ width: 480 }, { width: 960 }]
+  }
+});
 
 export default class Basic extends React.Component {
   render() {
@@ -11,7 +19,7 @@ export default class Basic extends React.Component {
         contentType="video"
         title="How to eject a Style Component in Mapbox Studio"
         vimeoId="378704089"
-        vimeoThumbnail="../files/vimeo_thumb.jpg"
+        vimeoThumbnail={<AppropriateImage imageId="vimeo-thumb" />}
       >
         Style Components provide sensible defaults and quick opportunities for
         customization by optimizing the most common property changes for styles
