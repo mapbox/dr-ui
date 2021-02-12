5b95464521180559c1c3e53f5f124d1390903766
Katy DeCorah <decorah@mapbox.com>
2021-02-12 13:47:52 -0500

    Updates

--- assets/vendor-cc0c93556f391910bb91.chunk.js
+++ assets/vendor-cc0c93556f391910bb91.chunk.js
@@ -1,4 +1,4 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[26],{"+wdc":function(e,t,n){"use strict";
+(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{"+wdc":function(e,t,n){"use strict";
 /** @license React v0.19.1
  * scheduler.production.min.js
  *
