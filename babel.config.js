609cd8dd228de8b6ec57945b2a2118f557125cb7
Katy DeCorah <decorah@mapbox.com>
2021-04-08 14:47:07 -0400

    Define development babel env to prevent `@loadable/babel-plugin` from loading on start (because it fails)

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
-'use strict';
+const { NODE_ENV } = process.env;
+
+const inDevelopment = NODE_ENV === 'development';
 
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: ['@babel/plugin-proposal-class-properties', '@loadable/babel-plugin']
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    !inDevelopment && '@loadable/babel-plugin' // this plugin fails on `npm start`
+  ].filter(Boolean)
 };
