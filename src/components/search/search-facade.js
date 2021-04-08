d2e7fd92e6b5d13a884a85dbdb1a05a3895af001
Katy DeCorah <decorah@mapbox.com>
2021-04-08 14:49:33 -0400

    Adjusting snapshots

--- src/components/search/search-facade.js
+++ src/components/search/search-facade.js
@@ -10,17 +10,21 @@ export class SearchFacade extends React.PureComponent {
     if (this.props.resultsOnly) return <div />;
     return (
       <div className="h36 relative">
-        <button
-          className="w-full block"
-          onClick={this.props.loadSearch}
-          {...buttonProps}
-        >
-          {this.props.useModal ? (
-            <SearchButton {...this.props} />
-          ) : (
-            <SearchInput {...this.props} />
-          )}
-        </button>
+        <div>
+          <div>
+            <button
+              className="w-full block"
+              onClick={this.props.loadSearch}
+              {...buttonProps}
+            >
+              {this.props.useModal ? (
+                <SearchButton {...this.props} />
+              ) : (
+                <SearchInput {...this.props} />
+              )}
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
