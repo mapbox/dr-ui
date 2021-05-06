c3e4a82af2a156a006da36b2900dcc68ea5d1532
Katy DeCorah <decorah@mapbox.com>
2021-05-06 09:12:55 -0400

    Fix siteOrigin

--- .remarkrc.js
+++ .remarkrc.js
@@ -1,7 +1,7 @@
 const rc = require('@mapbox/remark-config-docs');
 const config = {
   siteBasePath: 'dr-ui',
-  siteOrigin: 'https://mapbox.github.com',
+  siteOrigin: 'https://mapbox.github.io',
   pages: 'docs/src/pages/',
   constants: 'docs/src/constants.json',
   ignoreLinks: 'conf/ignore-links.json'
