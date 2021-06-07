6c73f65025a6fa77a7d0c60cdca198ef07db5016
Katy DeCorah <decorah@mapbox.com>
2021-06-07 11:22:27 -0400

    Add babel-plugin-transform-react-remove-prop-types to babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,8 @@
 
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: ['@babel/plugin-proposal-class-properties']
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    'babel-plugin-transform-react-remove-prop-types'
+  ]
 };
