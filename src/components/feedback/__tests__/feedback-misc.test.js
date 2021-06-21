7610e5d38fbbb905ef9912e3dd7dc9506adcf28d
Katy DeCorah <decorah@mapbox.com>
2021-06-21 16:36:39 -0400

    Remove redundant test

--- src/components/feedback/__tests__/feedback-misc.test.js
+++ src/components/feedback/__tests__/feedback-misc.test.js
@@ -3,43 +3,6 @@ import Feedback, { returnGenericType } from '../feedback';
 import { mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
-test('Feedback too long', () => {
-  const feedback = mount(
-    <Feedback
-      site="dr-ui"
-      location={{
-        pathname: '/mapbox-gl-js/api/',
-        hash: '#lnglat'
-      }}
-      webhook={{
-        production: '',
-        staging: ''
-      }}
-    />
-  );
-
-  feedback.setState({
-    isOpen: true,
-    category: 'Something is confusing'
-  });
-  const textarea = feedback.find('textarea');
-  textarea.simulate('change', {
-    target: {
-      value:
-        "I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise. I found a sandwich and I want to know why there isn't any mayonnaise."
-    }
-  });
-  const feedbackLength = feedback.find('#feedback-length');
-  expect(feedbackLength.text()).toEqual('-1099');
-  const overLimit = feedback.find('#feedback-overlimit');
-  expect(overLimit.text()).toEqual(
-    ' Your message is over the 1000 character limit.'
-  );
-  // Submit button is disabled when user enters too long text
-  const submitBtn = feedback.find('#dr-ui--feedback-submit-button');
-  expect(submitBtn.props().disabled).toBeTruthy();
-});
-
 test('Generate snapshot for feedback confirmation', () => {
   const feedback = mount(
     <Feedback
