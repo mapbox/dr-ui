a4ec98f9e3835268d9d9d10fd5264ea218b28b17
colleenmcginnis <colleen.mcginnis@mapbox.com>
2021-06-28 13:53:57 -0500

    style grouped guide index for narrow screens

--- src/components/page-layout/components/guide-group-index.js
+++ src/components/page-layout/components/guide-group-index.js
@@ -12,18 +12,19 @@ export default class GuideGroupIndex extends React.PureComponent {
         {subPages &&
           subPages.map((subpage, i) => {
             return (
-              <div key={subpage.title}
+              <div
+                key={subpage.title}
                 className={classnames(
-                  'flex-parent flex-parent--start-cross mb12',
+                  'flex-parent-ml flex-parent--start-cross mb12',
                   {
                     'border-b border--gray-light pb12': i < subPages.length - 1
                   }
                 )}
               >
-                <div className="flex-child txt-bold w240 flex-child--no-shrink">
+                <div className="flex-child-ml flex-child--no-shrink txt-bold w240 my0-ml mt18 mb12">
                   <a href={subpage.path}>{subpage.title}</a>
                 </div>
-                <div className="flex-child flex-child--grow">
+                <div className="flex-child-ml flex-child--grow">
                   {subpage.description}
                 </div>
               </div>
