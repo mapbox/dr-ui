08af31e2ff43b763affb9bdb06987623c9b59be9
Katy DeCorah <decorah@mapbox.com>
2021-07-23 10:01:32 -0400

    Configure jest to handle es6 modules

--- babel.config.js
+++ babel.config.js
@@ -5,5 +5,11 @@ module.exports = {
   plugins: [
     '@babel/plugin-proposal-class-properties',
     'babel-plugin-transform-react-remove-prop-types'
-  ]
+  ],
+  env: {
+    // When running `test` Jest will enable compilation from ECMAScript modules to CommonJS automatically
+    test: {
+      plugins: ['@babel/plugin-transform-modules-commonjs']
+    }
+  }
 };
