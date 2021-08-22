d9c7cac75fd2cc29c5b1141d0eb11f8680645f04
malwoodsantoro <mallorywood413@gmail.com>
2021-08-21 20:37:09 -0700

    remove passthroughProps conditional

--- src/components/download-button/download-button.js
+++ src/components/download-button/download-button.js
@@ -7,25 +7,15 @@ export default class DownloadButton extends React.PureComponent {
   render() {
     const { href, text } = this.props;
     const fileType = href.split('.').pop().toUpperCase();
-    const fileName = href
-      .split('/')
-      .pop()
-      .replace(/\.\w+$/, '');
-
-    console.log(fileType);
+    const fileName = href.split('/').pop();
 
     return (
       <Button
         href={href}
-        {...(fileType === 'CSV'
-          ? {}
-          : {
-              passthroughProps: {
-                download: fileName,
-                className:
-                  'unprose btn btn--blue round-full py12 px24 txt-s my18'
-              }
-            })}
+        passthroughProps={{
+          download: fileName,
+          className: 'unprose btn btn--blue round-full py12 px24 txt-s my18'
+        }}
       >
         <IconText iconBefore="arrow-down">
           {text || 'Download ' + fileType + ' file'}
