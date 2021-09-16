5b866f4c82acb5d1b46b4febcc901eeaa9353d5e
Katy DeCorah <decorah@mapbox.com>
2021-09-16 11:52:02 -0400

    [Assembly v1] ProductMenu

--- src/components/product-menu/product-menu.js
+++ src/components/product-menu/product-menu.js
@@ -4,46 +4,24 @@ import Tag from '../tag/tag';
 import classnames from 'classnames';
 
 class ProductMenu extends React.PureComponent {
-  buildTag = (item) => {
-    const tagProps = {
-      theme: item.tag,
-      customLabel: item.customTagProps
-        ? item.customTagProps.customLabel
-        : undefined,
-      customTooltipText: item.customTagProps
-        ? item.customTagProps.customTooltipText
-        : undefined,
-
-      customBackground: item.customTagProps
-        ? item.customTagProps.customBackground
-        : undefined,
-      customColor: item.customTagProps
-        ? item.customTagProps.customColor
-        : undefined,
-      customBorder: item.customTagProps
-        ? item.customTagProps.customBorder
-        : undefined
-    };
-    return (
-      <div className="ml-neg3">
-        <Tag {...tagProps} small={true} />
-      </div>
-    );
-  };
-
   render() {
-    const { props } = this;
+    const { tag, customTagProps, lightText, productName, homePage } =
+      this.props;
     return (
       <div className="dr-ui--product-menu">
-        {props.tag && this.buildTag(props)}
+        {tag && (
+          <div className="ml-neg3">
+            <Tag theme={tag} {...customTagProps} small={true} />
+          </div>
+        )}
         <a
-          href={props.homePage}
+          href={homePage}
           className={classnames('txt-fancy txt-l block', {
-            'color-white color-gray-light-on-hover': props.lightText,
-            'color-blue-on-hover': !props.lightText
+            'color-white color-gray-light-on-hover': lightText,
+            'color-blue-on-hover color-text': !lightText
           })}
         >
-          {props.productName}
+          {productName}
         </a>
       </div>
     );
