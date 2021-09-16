fabf27cd90b6ac230142987d10a0ae917f3a926e
Vincent Grafé <vincent.grafe@gmail.com>
2021-09-16 18:01:04 -0400

    adds `height` prop to `demo-iframe` component

--- src/components/demo-iframe/demo-iframe.js
+++ src/components/demo-iframe/demo-iframe.js
@@ -23,7 +23,7 @@ export default class DemoIframe extends React.PureComponent {
 
   render() {
     let { src } = this.props;
-    const { title, gl } = this.props;
+    const { title, gl, height } = this.props;
     const { mapboxAccessToken } = this.state;
     // check to see if the src is making a request to mapbox api
     const makeRequest = src.indexOf('MapboxAccessToken') > -1;
@@ -38,7 +38,7 @@ export default class DemoIframe extends React.PureComponent {
       ? src.replace('MapboxAccessToken', mapboxAccessToken)
       : src;
 
-    const iframeHeight = 400;
+    const iframeHeight = height || 400;
 
     const contents = (
       <div>
@@ -70,6 +70,8 @@ DemoIframe.propTypes = {
   gl: PropTypes.bool,
   /** Replace instance of `MapboxAccessToken` in the `src` prop with the value of this Mapbox access token. */
   MapboxAccessToken: PropTypes.string,
+  /** Height of the iframe in pixels. */
+  height: PropTypes.number,
   /** A title to describe the content of the iframe. */
   title: PropTypes.string.isRequired
 };
