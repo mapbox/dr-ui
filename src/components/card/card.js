713ea4be0d943f7a3137bb4bc57ca46ed70f91fa
Andrew Sepic <andrew.sepic@mapbox.com>
2023-12-04 21:11:58 -0500

    Added styles & hover state for card component

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -4,8 +4,22 @@ import IconText from '@mapbox/mr-ui/icon-text';
 
 import LevelIndicator from '../level-indicator/level-indicator';
 
+//        style={{ background: isHovered ? 'lightblue' : 'white' }}
+
 class Card extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHovered: false
+    };
+  }
   render() {
+    function toggleHover() {
+      this.setState((prevState) => ({
+        isHovered: !prevState.isHovered
+      }));
+    }
+    const { isHovered } = this.state;
     const { thumbnail, level, language, description, path, link, title } =
       this.props;
     const renderedThumbnail = thumbnail && (
@@ -31,20 +45,42 @@ class Card extends React.PureComponent {
 
     return (
       <a
-        className="dr-ui--card color-gray-dark transition color-blue-on-hover round clip inline-block w-full unprose pb18"
+        className="dr-ui--card text-color-gray-dark transition color-blue-on-hover round clip inline-block w-full unprose pb18"
         href={link || path}
+        onMouseEnter={toggleHover}
+        onMouseLeave={toggleHover}
         {...externalLinkAttributes}
       >
         {renderedThumbnail}
-        <div>
+        <div className="transition">
           {(renderedLevel || renderedLanguage) && (
             <div className="flex mb6 txt-bold color-gray txt-s">
               {renderedLevel}
               {renderedLanguage}
             </div>
           )}
-          <div className="mb6 flex flex--center-cross">
-            {title}{' '}
+          <div className="mb6 flex txt-bold flex--center-cross">
+            <div>{title} </div>
+            <div className="w60">
+              <svg
+                className="transition"
+                style={{
+                  marginLeft: isHovered ? '15px' : '10px',
+                  opacity: isHovered ? '1' : '0'
+                }}
+                width="11"
+                height="11"
+                viewBox="0 0 11 11"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M5.292 0L10.314 5.022V5.49L5.292 10.494L3.87 9.072L6.732 6.21L5.508 6.264H0V4.212H5.508L6.696 4.266L3.87 1.44L5.292 0Z"
+                  fill="#3478F3"
+                />
+              </svg>
+            </div>
+
             {link && (
               <svg className="icon ml3 mt3">
                 <use xlinkHref="#icon-share" />
@@ -52,7 +88,9 @@ class Card extends React.PureComponent {
             )}
           </div>
           {description && (
-            <div className="color-gray color-gray-on-hover">{description}</div>
+            <div style={{ color: isHovered ? '#23262D' : '#757d82' }}>
+              {description}
+            </div>
           )}
         </div>
       </a>
